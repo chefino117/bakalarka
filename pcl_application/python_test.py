68ae9da7201d955476860b7c7316bc06017e9202
chefino117 <hrabossimon@gmail.com>
2017-12-12 16:15:57 +0100

    aplikacia na zobrazenie trajektorie

---
+++ pcl_application/python_test.py
@@ -0,0 +1,13 @@
+import pcl
+import pypcd
+
+# also can read from file handles.
+pc = pypcd.PointCloud.from_path('trajektoria.pcd')
+# pc.pc_data has the data as a structured array
+# pc.fields, pc.count, etc have the metadata
+
+# center the x field
+pc.pc_data['x'] -= pc.pc_data['x'].mean()
+
+# save as binary compressed
+pc.save_pcd('bar.pcd', compression='binary_compressed')
